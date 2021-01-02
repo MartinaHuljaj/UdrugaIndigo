02d63950fa13c7713c74d5529951c86b0e242dd0
MartinaHuljaj <martina.huljaj@gmail.com>
2021-01-02 22:37:44 +0100

    naslovna slika

---
+++ src/components/HomeImage/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import styles from './style.module.css'
+
+const HomeImage = () =>(
+    <section className={styles.image}></section>
+)
+export default HomeImage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import React from "react"
 import Header from '../components/Header'
+import HomeImage from '../components/HomeImage'
 
 
 const IndexPage = ()=>(
   <main>
     <Header />
-    
+    <HomeImage/>
   </main>
 )
 
