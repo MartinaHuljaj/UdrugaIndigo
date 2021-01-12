6161184bc05cc4e0243346edaaf573dc6ee063ab
MartinaHuljaj <martina.huljaj@gmail.com>
2021-01-12 22:44:08 +0100

    naslovni clanak

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,10 +1,12 @@
 import Slika from '../Image'
 import naslovna from '../Image/naslovna1.jpg'
 import React from 'react'
+import styles from './style.module.css'
 
 const Home =()=>(
-    <section>
-       <Slika slika={naslovna} />
+    <section className={styles.container} >
+       <Slika  slika={naslovna}/>
+       <div className={styles.clanak}>Dobro došli na stranicu udruge za zaštitu životinja Indigo!<pre className={styles.pod}>→Upoznaj nas</pre></div>
     </section>
     )
 export default Home
