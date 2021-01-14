d0fc6e85d1fc409337f4340656a7b1077c61f500
LJanketic <luka.janketic@gmail.com>
2021-01-14 16:50:25 +0100

    Dodavanje sekcije "nasi ljubimci"

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,16 +2,31 @@ import Slika from '../Image'
 import cover from '../Image/naslovna1.jpg'
 import React from 'react'
 import styles from './style.module.css'
+import pic1 from './1.jpg'
+import pic2 from './2.jpg'
+import pic3 from './3.jpg'
+import pic4 from './4.jpg'
+import pic5 from './5.jpg'
 
 
 const Home =()=>(
-    <main className={styles.container}>
-
+    <main>
+    <section  className={styles.container}>
       <Slika  slika={cover}  />
-       <div className={styles.clanak}>Dobro došli na stranicu udruge za zaštitu životinja Indigo!<pre className={styles.pod}>→Upoznaj nas</pre></div>
-
+      <div className={styles.clanak}>Dobro došli na stranicu udruge za zaštitu životinja Indigo!<pre className={styles.pod}>→Upoznaj nas</pre></div>
+    </section>
+    <section className={styles.galerijaOkvir}>
+      <div className={styles.text}>Naši ljubimci: </div>
+      <div className={styles.galerija}><img src={pic1}></img></div>
+      <div className={styles.galerija}><img src={pic2}></img></div>
+      <div className={styles.galerija}><img src={pic3}></img></div>
+      <div className={styles.galerija}><img src={pic4}></img></div>
+      <div className={styles.galerija}><img src={pic5}></img></div>
+    </section>
+    <section>
+      <p></p>
+    </section>
     </main>
-    
     
     )
 export default Home
