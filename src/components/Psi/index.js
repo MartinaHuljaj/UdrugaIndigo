6637d7308d5a09c833c2ecc0c2398019e81cdefe
MartinaHuljaj <martina.huljaj@gmail.com>
2021-02-13 18:45:20 +0100

    Popravljen ssr

--- src/components/Psi/index.js
+++ src/components/Psi/index.js
@@ -1,59 +1,60 @@
 import React from 'react'
 import styles from './style.module.css'
-import {useStaticQuery, graphql, Link} from 'gatsby'
 import Img from 'gatsby-image'
 import cover from '../../images/2.png'
 import Image from '../Image'
+import {useQuery } from '@apollo/client'
+import gql from 'graphql-tag'
+import {Link} from 'gatsby'
 
-const Psi =()=>{
-    const data=useStaticQuery(graphql`
-    query {
-      allContentfulPsi (filter: { node_locale: { eq: "en-US" } }) {
-        nodes {
-            vaxine
-            title
-            slug
-            age
-            picture {
-              fixed(width: 300) {
-                aspectRatio
-                base64
-                height
-                src
-                srcSet
-                srcSetWebp
-                srcWebp
-                width
-              }
-            }
-            text {
-              raw
+
+
+const Psi = ()=>{
+  const get_data = gql`query{
+    allContentfulPsi (filter: { node_locale: { eq: "en-US" } }) {
+      nodes {
+          vaxine
+          title
+          slug
+          age
+          picture {
+            fixed(width: 300) {
+              aspectRatio
+              base64
+              height
+              src
+              srcSet
+              srcSetWebp
+              srcWebp
+              width
             }
           }
-      }
-    }`)
+          text {
+            raw
+          }
+        }
+    }
+  }`
+  const { loading, error, data }=useQuery(get_data)
+  if(loading)return 'Loading..'
+  if(error) return 'error'
 
-    return(
-        <main>
-            <Image slika={cover}/>
-            <section className={styles.container}>
-                <div className={styles.row}>
-                {data.allContentfulPsi.nodes.map(node => {
-                return (
-                    <Link to={`/psi/${node.slug}`}>
-                    <div className={styles.post}>
-                    <Img fixed={node.picture.fixed}  />
-                    <h3 className={styles.textBottom}>{node.title}/{node.age}</h3>
-                    </div>
-                    </Link>
-            )
-          })}  
-
-                </div>
-            </section>
-
-        </main>
-    )
+  return(
+    <main>
+      <Image slika={cover}/>
+      <section className={styles.container}>
+        {data.nodes.map(node=>{
+          return(
+            <Link to={`/psi/${node.slug}`}>
+            <div key={node.title} className={styles.post}>
+            <h3 className={styles.textBottom}>{node.title}/{node.age}</h3> 
+            </div>
+            </Link>
+          )
+        })}
+      </section>
+    </main>
+  )
 }
 
 export default Psi
